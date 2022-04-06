4140caf137cc56c5ff8d1979b04d194575b92f77
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-04-06 09:51:48 -0700

    finish sixth exercise

---
+++ ex6.py
@@ -0,0 +1,36 @@
+# creates a variable for types of people
+types_of_people = 10
+# creates a variable that prints a messgae
+x = f"There are {types_of_people} types of people."
+
+# creates the variable binary and sets it to a string
+binary = "binary"
+# creates the do_not variable and sets it to don't
+do_not = "don't"
+# creates a variable y and prints a message
+y = f"Those who know {binary} and those who {do_not}."
+
+# prints the variable x
+print(x)
+# prints the variable y
+print(y)
+
+# prints a formatted message with the variable x
+print(f"I said: {x}")
+# prints a formatted message with the variable y
+print(f"I also said: '{y}'")
+
+# creates a variable hilarious and sets it to false
+hilarious = False
+# creates a variable joke_evaluation and sets it to a message
+joke_evaluation = "Isn't that joke so funny?! {}"
+# prints a formatted string with the variable joke_evaluation
+print(joke_evaluation.format(hilarious))
+
+# sets w to a string
+w = "This is the left side of..."
+# sets e to a string
+e = "a string with a right side."
+
+# combines the variables w and e and prints them out
+print(w + e)
