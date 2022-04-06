1ad32f9e15394fdfd74c052724a6076b782e8aea
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-04-06 09:30:10 -0700

    finish part b of exercise 3

---
+++ ex3b.py
@@ -0,0 +1,38 @@
+# displays a message
+print("I will now count my chickens:")
+
+# calculates number of hens
+print("Hens", 25.0 + 30.0 / 6.0)
+# calculates number of roosters
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# displays a message
+print("Now I will count the eggs:")
+
+# calculates number of eggs
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# displays a message
+print("is it true that 3 + 2 < 5 - 7?")
+
+# performs a calculation and then comparison
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# displays the answer of the calculation
+print("What is 3 + 2?", 3.0 + 2.0)
+
+# also displays the answer
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# displays a message
+print("Oh, that's why it's False.")
+
+# displays another message
+print("How about some more.")
+
+# displays the answer
+print("Is it greater?", 5.0 > -2.0)
+# displays the answer
+print("Is it greater or equal?", 5.0 >= -2.0)
+# displays the answer
+print("Is it less or equal?", 5.0 <= -2.0)
