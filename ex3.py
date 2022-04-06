414afcfe7dfdaf561aed5a95925240c2a158e352
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-04-06 09:29:20 -0700

    finish third exercise

---
+++ ex3.py
@@ -0,0 +1,38 @@
+# displays a message
+print("I will now count my chickens:")
+
+# calculates number of hens
+print("Hens", 25 + 30 / 6)
+# calculates number of roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+# displays a message
+print("Now I will count the eggs:")
+
+# calculates number of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# displays a message
+print("is it true that 3 + 2 < 5 - 7?")
+
+# performs a calculation and then comparison
+print(3 + 2 < 5 - 7)
+
+# displays the answer of the calculation
+print("What is 3 + 2?", 3 + 2)
+
+# also displays the answer
+print("What is 5 - 7?", 5 - 7)
+
+# displays a message
+print("Oh, that's why it's False.")
+
+# displays another message
+print("How about some more.")
+
+# displays the answer
+print("Is it greater?", 5 > -2)
+# displays the answer
+print("Is it greater or equal?", 5 >= -2)
+# displays the answer
+print("Is it less or equal?", 5 <= -2)
